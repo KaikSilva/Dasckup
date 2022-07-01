774099d0e704c98d48ab3c6b4434c4ee418d9986
Kaik Silva <kaikrochaa0@gmail.com>
2022-07-01 14:43:07 -0300

    Updates

---
+++ assets/web/js/index.js
@@ -0,0 +1,6 @@
+import $ from "jquery/dist/jquery.min";
+require("assets/web/plugins/simplebar/js/");
+require("assets/web/plugins/metismenu/js/");
+require("assets/web/plugins/perfect-scrollbar/js");
+require("assets/web/js/app");
+
